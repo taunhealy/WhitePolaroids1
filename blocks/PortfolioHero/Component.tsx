6365d36f36325ea172e05490e1f13436effb8c84
taunhealy <taunhealy@gmail.com>
2023-04-19 19:11:29 +0200

    Portfolio Block update

---
+++ blocks/PortfolioHero/Component.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { MediaType } from '../../collections/Media'
+
+
+export type Type = {
+    blockType: 'portfoliohero'
+    blockName?: string
+    thumbnail: MediaType
+    title: string;
+}
+  
+export const Component: React.FC<Type> = (props) => {
+    const { thumbnail, title } = props;
+  
+    return (
+      <div>
+        <img src={thumbnail.url} alt={thumbnail.alt} />
+        <h1>{title}</h1>
+      </div>
+    );
+  }
--- blocks/index.ts
+++ blocks/index.ts
@@ -4,15 +4,23 @@ import { Content } from './Content/Config';
 import { Component as content } from './Content/Component';
 import { Image } from './Image/Config';
 import { Component as image } from './Image/Component';
+import { PortfolioHero } from './PortfolioHero/Config';
+import { Component as portfoliohero } from './PortfolioHero/Component';
+
+// import the collections Configs to export as 'collections'.
 
 export const collections = {
   CallToAction,
   Content,
   Image,
+  PortfolioHero,
+
 };
 
 export const components = {
   cta,
   content,
   image,
+  portfoliohero
 };
+
